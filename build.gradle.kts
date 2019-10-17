2feee0c0ce7fc8cc20f9ade010475559f0dae0d4
Julien Terrier <legzomil@gmail.com>
2019-10-17 20:53:07 +0200

    Fixes the heroku deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,22 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.3.50"
+    id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
 group = "com.jtelabs"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    jcenter()
 }
 
-val http4k_version = "3.189.0"
+val http4kVersion = "3.189.0"
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.http4k:http4k-core:$http4k_version")
-    implementation("org.http4k:http4k-server-jetty:$http4k_version")
+    implementation("org.http4k:http4k-core:$http4kVersion")
+    implementation("org.http4k:http4k-server-jetty:$http4kVersion")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("com.github.dpaukov:combinatoricslib3:3.3.0")
     implementation("org.http4k:http4k-core:3.189.0")
@@ -29,3 +30,8 @@ val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions {
     freeCompilerArgs = listOf("-XXLanguage:+InlineClasses")
 }
+
+tasks.register("stage") {
+    dependsOn("clean", "shadowJar")
+}
+
